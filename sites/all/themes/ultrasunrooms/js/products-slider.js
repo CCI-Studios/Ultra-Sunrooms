31efb150878f7f7805ef5f6c7122d534f6ff5f79
Hemant Sanvaria <hsanvaria@ccistudios.com>
2016-05-05 16:16:53 -0400

    hide empty subtitle products page

--- sites/all/themes/ultrasunrooms/js/products-slider.js
+++ sites/all/themes/ultrasunrooms/js/products-slider.js
@@ -1,81 +1,75 @@
 (function($){
 
-$(function(){
-var j=1;
-	var active=[];
+	$(function(){
+	var j = 1;
+	var active = [];
 	var min = [];
 	var max =[];
-$('.item-list li > a').swipebox();
-var max1= $('.view-products-page .views-row').length;
+	$('.item-list li > a').swipebox();
+	var max1 = $('.view-products-page .views-row').length;
 
-       for(j=1;j<=max1;j++)
-       {
-       		active[j]=0;
-			min[j]=0;
-			max[j]=0;
-			   
-			init(j);	
-       }
+	$('.view-products-page .views-row').each(function(index){
+		console.log($(this).find('.subtitle p').text());
+		if($(this).find('.subtitle p').text() == " "){
+			$(this).find('.subtitle p').hide();
+		}
+	});
+	
 
-    function init(j)
-	{	
-	
-	}
-	
 	$('.item-list ul').slick({
-		dots: false,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 6,
-  slidesToScroll: 6,
-  responsive: [
-   {
-      breakpoint: 1400,
-      settings: {
-        slidesToShow: 5,
-        slidesToScroll: 5,
-        infinite: true,
-        dots: false
-      }
-    },
-    {
-      breakpoint: 1120,
-      settings: {
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        infinite: true,
-        dots: false
-      }
-    },
-    {
-      breakpoint: 980,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: false
-      }
-    },
-  
-    {
-      breakpoint: 767,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]});
+	  dots: false,
+	  infinite: true,
+	  speed: 300,
+	  slidesToShow: 6,
+	  slidesToScroll: 6,
+	  responsive: [
+	   {
+	      breakpoint: 1400,
+	      settings: {
+	        slidesToShow: 5,
+	        slidesToScroll: 5,
+	        infinite: true,
+	        dots: false
+	      }
+	    },
+	    {
+	      breakpoint: 1120,
+	      settings: {
+	        slidesToShow: 4,
+	        slidesToScroll: 4,
+	        infinite: true,
+	        dots: false
+	      }
+	    },
+	    {
+	      breakpoint: 980,
+	      settings: {
+	        slidesToShow: 3,
+	        slidesToScroll: 3,
+	        infinite: true,
+	        dots: false
+	      }
+	    },
+	  
+	    {
+	      breakpoint: 767,
+	      settings: {
+	        slidesToShow: 2,
+	        slidesToScroll: 2
+	      }
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	        slidesToShow: 1,
+	        slidesToScroll: 1
+	      }
+	    }
+	    // You can unslick at a given breakpoint now by adding:
+	    // settings: "unslick"
+	    // instead of a settings object
+	  ]});
 
-});
+	});
 
 })(jQuery);
